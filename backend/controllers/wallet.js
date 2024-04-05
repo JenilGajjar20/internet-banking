10b50a9cf873a1de57226e7188cc926851ca4130
Jenil Gajjar <jenil20gajjar@gmail.com>
2024-04-05 10:39:20 +0530

    wallet WIP

--- backend/controllers/wallet.js
+++ backend/controllers/wallet.js
@@ -3,33 +3,52 @@ const Wallet = require("../models/Wallet");
 
 const createWallet = async (req, res) => {
   try {
-    const { customerId } = req.body;
+    const customerId = req.body.customer.id;
+    // const customerEmail = req.body.customer.email;
 
     const customer = await Customer.findById(customerId);
+    // console.log("customer: ", customer);
     // Check if the customer exists
-    if (customer) {
+    if (!customer) {
       return res.status(404).json({ message: "Customer not found" });
     }
 
-    const existingWallet = await Wallet.findOne({ customer: customerId });
+    // const existingWallet = await Wallet.findOne({
+    //   customer: { id: req.body.customer.id },
+    // });
+    console.log("c_id: ", req.body.customer.id);
     // Check if the customer has already created a wallet
-    if (existingWallet) {
-      return res.status(400).json({ message: "Customer already has a wallet" });
-    }
+    // if (existingWallet) {
+    //   return res.status(400).json({ message: "Customer already has a wallet" });
+    // }
 
     // Creating a new wallet
-    const newWallet = await Wallet({ customer: customerId, balance: 0 });
+    // const newWallet = await Wallet({
+    //   customer: { id: customerId, email: customerEmail },
+    //   balance: 0,
+    // });
+    // console.log("newWallet: ", newWallet);
 
-    const saveWallet = await newWallet.save();
+    // const saveWallet = await newWallet.save();
 
-    return res
-      .status(201)
-      .json({ data: saveWallet, message: "Wallet created successfully" });
+    // return res
+    //   .status(201)
+    //   .json({ data: saveWallet, message: "Wallet created successfully" });
   } catch (err) {
-    return res
-      .status(500)
-      .json({ error: err, message: "Oops! Something went wrong" });
+    console.log("error", err);
+    // return res
+    //   .status(500)
+    //   .json({ error: err, message: "Oops! Something went wrong" });
   }
 };
+
+// const getWalletData = async (req, res) => {
+//   try {
+//     // const walletData = await Wallet.findById({});
+//     console.log("Wallet data: ", req);
+//   } catch (err) {
+//     console.log("Error: ", err);
+//   }
+// };
 
 module.exports = { createWallet };
--- backend/routes/wallet.js
+++ backend/routes/wallet.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 
-const { createWallet } = require("../controllers/wallet");
+const { createWallet, getWalletData } = require("../controllers/wallet");
 
 const authMiddleware = require("../middlewares/auth");
 
 router.post("/new", authMiddleware, createWallet);
+
+// router.get("/:id", authMiddleware, getWalletData);
 
 module.exports = router;
